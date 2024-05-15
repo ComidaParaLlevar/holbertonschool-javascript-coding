02b3f0ee80663558402ce8ed2592e3d61ad237ec
Lyan <lyanosorio98@gmail.com>
2024-05-15 16:41:14 -0400

    converted function into arrow function

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,8 @@
 const fs = require('fs');
 
 const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, content) {
+
+fs.readFile(file, 'utf-8', (err, content) => {
   if (err) {
     console.log(err);
   } else { console.log(content); }
